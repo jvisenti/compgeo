669bdf469e6e102a5a2489d678437b1cb29ca617
Rob Visentin <jvisenti@gmail.com>
2014-04-05 10:41:35 -0400

    better way of computing bounding spheres

--- src/MPMesh.c
+++ src/MPMesh.c
@@ -11,9 +11,7 @@
 
 #pragma mark - private definitions
 
-const int kNumExtrema = 6;
-
-MPVec3** _MPMeshFindExtrema(MPMesh *mesh);
+MPSphere _MPMeshComputeBoundingSphere(MPMesh *mesh);
 
 #pragma mark - public functions
 
@@ -31,7 +29,8 @@ MPMesh* MPMeshCreate(const MPVec3 *vertexData, size_t stride, size_t numVertices
     mesh->indexSize = indexSize;
     mesh->numIndices = numIndices;
     
-    mesh->_reserved = _MPMeshFindExtrema(mesh);
+    mesh->_reserved = malloc(sizeof(MPSphere));
+    *(MPSphere *)(mesh->_reserved) = _MPMeshComputeBoundingSphere(mesh);
     
     return mesh;
 }
@@ -73,49 +72,41 @@ void MPMeshRelease(MPMesh *mesh)
 
 MPSphere MPMeshGetBoundingSphere(const MPMesh *mesh, const MPMat4 *transform)
 {    
-    MPVec3 **extrema = (MPVec3 **)mesh->_reserved;
-    
-    MPVec3 left     = *extrema[0];
-    MPVec3 right    = *extrema[1];
-    
-    MPVec3 bottom   = *extrema[2];
-    MPVec3 top      = *extrema[3];
-    
-    MPVec3 back     = *extrema[4];
-    MPVec3 front    = *extrema[5];
+    MPSphere boundingSphere = *(MPSphere *)(mesh->_reserved);
     
     if (transform != NULL)
     {
-        left = MPMat4TransformVec3(*transform, left);
-        right = MPMat4TransformVec3(*transform, right);
+        // apply translation to center point (scale and rotation don't matter)
+        boundingSphere.center.x -= transform->m[12];
+        boundingSphere.center.y -= transform->m[13];
+        boundingSphere.center.z -= transform->m[14];
         
-        bottom = MPMat4TransformVec3(*transform, bottom);
-        top = MPMat4TransformVec3(*transform, top);
+        // scale the radius on the x-axis
+        MPVec3 xRad = MPVec3Make(boundingSphere.radius, 0.0f, 0.0f);
+        xRad = MPMat4TransformVec3(*transform, xRad);
+        xRad.x -= transform->m[12]; xRad.y -= transform->m[13]; xRad.z -= transform->m[14];
         
-        back = MPMat4TransformVec3(*transform, back);
-        front = MPMat4TransformVec3(*transform, front);
+        // scale the radius on the y-axis
+        MPVec3 yRad = MPVec3Make(0.0f, boundingSphere.radius, 0.0f);
+        yRad = MPMat4TransformVec3(*transform, yRad);
+        yRad.x -= transform->m[12]; yRad.y -= transform->m[13]; yRad.z -= transform->m[14];
+
+        // scale the radius on the z-axis
+        MPVec3 zRad = MPVec3Make(0.0f, 0.0f, boundingSphere.radius);
+        zRad = MPMat4TransformVec3(*transform, zRad);
+        zRad.x -= transform->m[12]; zRad.y -= transform->m[13]; zRad.z -= transform->m[14];
+        
+        // take the new radius to be the max of the radius scaled on each axis
+        boundingSphere.radius = fmaxf(MPVec3Length(xRad), MPVec3Length(yRad));
+        boundingSphere.radius = fmaxf(boundingSphere.radius, MPVec3Length(zRad));
     }
     
-    float midX = left.x + 0.5f * (right.x - left.x);
-    float midY = bottom.y + 0.5f * (top.y - bottom.y);
-    float midZ = back.z + 0.5f * (front.z - back.z);
-    
-    MPVec3 center = MPVec3Make(midX, midY, midZ);
-    
-    float maxDist = fmaxf(MPVec3EuclideanDistance(center, left), MPVec3EuclideanDistance(center, right));
-    
-    maxDist = fmaxf(maxDist, MPVec3EuclideanDistance(center, bottom));
-    
-    maxDist = fmaxf(maxDist, MPVec3EuclideanDistance(center, top));
-    maxDist = fmaxf(maxDist, MPVec3EuclideanDistance(center, back));
-    maxDist = fmaxf(maxDist, MPVec3EuclideanDistance(center, front));
-    
-    return MPSphereMake(center, maxDist);
+    return  boundingSphere;
 }
 
 #pragma mark - private functions
 
-MPVec3** _MPMeshFindExtrema(MPMesh *mesh)
+MPSphere _MPMeshComputeBoundingSphere(MPMesh *mesh)
 {
     MPVec3 *left, *right, *bottom, *top, *back, *front;
     left = right = bottom = top = back = front = (MPVec3 *)mesh->vertexData;
@@ -135,11 +126,19 @@ MPVec3** _MPMeshFindExtrema(MPMesh *mesh)
         if (current->z > front->z)  front = current;
     }
     
-    MPVec3 **extrema = malloc(kNumExtrema * sizeof(MPVec3 *));
+    float midX = left->x + 0.5f * (right->x - left->x);
+    float midY = bottom->y + 0.5f * (top->y - bottom->y);
+    float midZ = back->z + 0.5f * (front->z - back->z);
     
-    extrema[0] = left;     extrema[1] = right;
-    extrema[2] = bottom;   extrema[3] = top;
-    extrema[4] = back;     extrema[5] = front;
+    MPVec3 center = MPVec3Make(midX, midY, midZ);
     
-    return extrema;
+    float maxDist = fmaxf(MPVec3EuclideanDistance(center, *left), MPVec3EuclideanDistance(center, *right));
+    
+    maxDist = fmaxf(maxDist, MPVec3EuclideanDistance(center, *bottom));
+    
+    maxDist = fmaxf(maxDist, MPVec3EuclideanDistance(center, *top));
+    maxDist = fmaxf(maxDist, MPVec3EuclideanDistance(center, *back));
+    maxDist = fmaxf(maxDist, MPVec3EuclideanDistance(center, *front));
+    
+    return MPSphereMake(center, maxDist);
 }
